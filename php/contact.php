57e1a3e3a976d29ab7a74cd9bb8954c0f777e904
zkgohst-13 <67800575+zkgohst-13@users.noreply.github.com>
2023-02-21 20:56:33 +0100

    Add files via upload

---
+++ php/contact.php
@@ -0,0 +1,94 @@
+<?php
+
+    $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "message" => "", "firstnameError" => "", "nameError" => "", "emailError" => "", "phoneError" => "", "messageError" => "", "isSuccess" => false);
+    $emailTo = "badaouiislem440@gmail.com";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    { 
+        $array["firstname"] = test_input($_POST["firstname"]);
+        $array["name"] = test_input($_POST["name"]);
+        $array["email"] = test_input($_POST["email"]);
+        $array["phone"] = test_input($_POST["phone"]);
+        $array["message"] = test_input($_POST["message"]);
+        $array["isSuccess"] = true; 
+        $emailText = "";
+        
+        if (empty($array["firstname"]))
+        {
+            $array["firstnameError"] = "I WANT TO KNOW YOUR FIRSTNAME !";
+            $array["isSuccess"] = false; 
+        } 
+        else
+        {
+            $emailText .= "Firstname: {$array['firstname']}\n";
+        }
+
+        if (empty($array["name"]))
+        {
+            $array["nameError"] = "PLEASE ENTER YOUR LAST NAME !";
+            $array["isSuccess"] = false; 
+        } 
+        else
+        {
+            $emailText .= "Name: {$array['name']}\n";
+        }
+
+        if(!isEmail($array["email"])) 
+        {
+            $array["emailError"] = "PLEASE INSERT A VALID EMAIL  !";
+            $array["isSuccess"] = false; 
+        } 
+        else
+        {
+            $emailText .= "Email: {$array['email']}\n";
+        }
+
+        if (!isPhone($array["phone"]))
+        {
+            $array["phoneError"] = "ONLY NUMBERS OR SPACES ARE REQUIRED";
+            $array["isSuccess"] = false; 
+        }
+        else
+        {
+            $emailText .= "Phone: {$array['phone']}\n";
+        }
+
+        if (empty($array["message"]))
+        {
+            $array["messageError"] = "WHAT DO YOU WANT TO TELL ME ??";
+            $array["isSuccess"] = false; 
+        }
+        else
+        {
+            $emailText .= "Message: {$array['message']}\n";
+        }
+        
+        if($array["isSuccess"]) 
+        {
+            $headers = "From: {$array['firstname']} {$array['name']} <{$array['email']}>\r\nReply-To: {$array['email']}";
+            mail($emailTo, "a message from your site", $emailText, $headers);
+        }
+        
+        echo json_encode($array);
+        
+    }
+
+    function isEmail($email) 
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+    function isPhone($phone) 
+    {
+        return preg_match("/^[0-9 ]*$/",$phone);
+    }
+    function test_input($data) 
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+ 
+?>
+
+
